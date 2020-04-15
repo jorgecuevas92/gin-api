f36b2c5600ad2f3b8aef224d8936429d74a66451
Jorge Cuevas <jorge.cuevas.92@gmail.com>
2020-04-15 10:41:58 -0500

    Added Comments

--- example.go
+++ example.go
@@ -9,14 +9,16 @@ import (
 )
 
 func main() {
+	// Set the port
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
-	r := gin.New()
+	r := gin.Default()
 
+	// Define Handlers
 	rootHandler := func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World")
 	}
@@ -27,8 +29,11 @@ func main() {
 		})
 	}
 
+	// Configure Routes
 	r.GET("/", rootHandler)
 	r.GET("/ping", pingHandler)
+
+	// Run
 	log.Printf("Listening on port %s", port)
 	r.Run(":" + port)
 }
