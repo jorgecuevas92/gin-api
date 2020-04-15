57305c8b13cb3d8bde74610203ecc4d3c530d4f1
Jorge Cuevas <jorge.cuevas.92@gmail.com>
2020-04-15 10:37:54 -0500

    Configure port added logs and separated handlers

--- example.go
+++ example.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+	r := gin.New()
+
+	rootHandler := func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	}
+
+	pingHandler := func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
-	})
-	r.Run()
+	}
+
+	r.GET("/", rootHandler)
+	r.GET("/ping", pingHandler)
+	log.Printf("Listening on port %s", port)
+	r.Run(":" + port)
 }
